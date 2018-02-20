3f5e1019a6391e87e61c9f5e30d536f736626abf
kuswandanu <kuswandanu@gmail.com>
2018-02-20 15:36:37 +0700

    migrate to node-excel-export 1.4.2

--- example/index.js
+++ example/index.js
@@ -2,7 +2,74 @@ var json2xls = require('../lib/json2xls');
 var data = require('../spec/arrayData.json');
 var fs = require('fs');
 
-var xls = json2xls(data,{});
+
+var dataset = [
+    { customer_name: 'IBM', status_id: 1, note: 'some note', misc: 'not shown' },
+    { customer_name: 'HP', status_id: 0, note: 'some note' },
+    { customer_name: 'MS', status_id: 0, note: 'some note', misc: 'not shown' }
+];
 
 
-fs.writeFileSync('output.xlsx',xls, 'binary');
+const styles = {
+    headerDark: {
+        fill: {
+            fgColor: {
+                rgb: 'FF000000'
+            }
+        },
+        font: {
+            color: {
+                rgb: 'FFFFFFFF'
+            },
+            sz: 14,
+            bold: true,
+            underline: true
+        }
+    },
+    cellPink: {
+        fill: {
+            fgColor: {
+                rgb: 'FFFFCCFF'
+            }
+        }
+    },
+    cellGreen: {
+        fill: {
+            fgColor: {
+                rgb: 'FF00FF00'
+            }
+        }
+    }
+};
+
+var options = {};
+options.specification = {
+    customer_name: {
+        displayName: 'Customer',
+        headerStyle: styles.headerDark,
+        cellStyle: function (value, row) {
+            return (row.status_id == 1) ? styles.cellGreen : { fill: { fgColor: { rgb: 'FFFF0000' } } };
+        },
+        width: 120
+    },
+    status_id: {
+        displayName: 'Status',
+        headerStyle: styles.headerDark,
+        cellFormat: function (value, row) {
+            return (value == 1) ? 'Active' : 'Inactive';
+        },
+        width: '10'
+    },
+    note: {
+        displayName: 'Description',
+        headerStyle: styles.headerDark,
+        cellStyle: styles.cellPink,
+        width: 220
+    }
+};
+
+var xls = json2xls(dataset, options);
+// var xls = json2xls(data, {});
+
+
+fs.writeFileSync(__dirname + '/output.xlsx', xls, 'binary');
